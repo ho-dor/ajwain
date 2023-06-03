1c4a4cd183b79225b44059ac6058a9134aaa504f
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-06-03 20:48:47 +0530

    fix: submodule issue

---
+++ client/src/components/auth/loginForm.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { Container, Box, TextField, Button, Typography } from '@mui/material';
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Implement your login logic here
+    console.log(`Logging in with email: ${email} and password: ${password}`);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Sign In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginForm;
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/auth/loginForm';
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default LoginPage;
